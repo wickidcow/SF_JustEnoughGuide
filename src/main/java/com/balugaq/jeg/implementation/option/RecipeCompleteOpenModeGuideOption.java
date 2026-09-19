bedfbce1a703cc595674ea60a4235b52e0a4d1b7
wickidcow <jaywago@gmail.com>
2026-09-19 10:55:34 -0400

    chore: translate recipe completion open mode option

--- src/main/java/com/balugaq/jeg/implementation/option/RecipeCompleteOpenModeGuideOption.java
+++ src/main/java/com/balugaq/jeg/implementation/option/RecipeCompleteOpenModeGuideOption.java
@@ -51,9 +51,9 @@ public class RecipeCompleteOpenModeGuideOption extends AbstractBooleanGuideOptio
     public ItemStack getDisplayItem(Player p, ItemStack guide, boolean enabled) {
         return Converter.getItem(
             isEnabled(p) ? Material.ENCHANTED_BOOK : Material.KNOWLEDGE_BOOK,
-            "&b配方补全打开模式: &" + (enabled ? "4继承上一次打开的界面" : "a打开新界面"),
+            "&bRecipe Completion Open Mode: &" + (enabled ? "4Reuse Previous Guide" : "aOpen Fresh Guide"),
             "",
-            "&7\u21E8 &e点击切换配方补全打开模式为 " + (enabled ? "打开新界面" : "继承上一次打开的界面")
+            "&7\u21E8 &e点击切换配方补全打开模式为 " + (enabled ? "Open Fresh Guide" : "Reuse Previous Guide")
         );
     }
 
