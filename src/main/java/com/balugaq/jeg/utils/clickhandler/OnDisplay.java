ab1ac6e53d2361cdaf724df42d18648011412cc4
wickidcow <jaywago@gmail.com>
2026-09-19 11:40:50 -0400

    core: use local ItemStack display-name helper

--- src/main/java/com/balugaq/jeg/utils/clickhandler/OnDisplay.java
+++ src/main/java/com/balugaq/jeg/utils/clickhandler/OnDisplay.java
@@ -42,7 +42,7 @@ import io.github.thebusybiscuit.slimefun4.utils.ChestMenuUtils;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.ChestMenu;
-import net.guizhanss.minecraft.guizhanlib.gugu.minecraft.helpers.inventory.ItemStackHelper;
+import com.balugaq.jeg.utils.ItemStackHelper;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
