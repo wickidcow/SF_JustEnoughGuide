b2daca1ef4b17cf6c72a3d2574132477c78295a3
wickidcow <jaywago@gmail.com>
2026-09-19 11:05:11 -0400

    chore: translate LogiTech FALSE recipe option

--- src/main/java/com/balugaq/jeg/core/integrations/logitech/LogiTechFalseRecipeSettingsGuideOption.java
+++ src/main/java/com/balugaq/jeg/core/integrations/logitech/LogiTechFalseRecipeSettingsGuideOption.java
@@ -62,10 +62,10 @@ public class LogiTechFalseRecipeSettingsGuideOption extends AbstractItemSettings
         var sf = SlimefunItem.getById("LOGITECH_FALSE_");
         ItemStack item = sf != null ? Converter.getItem(
             sf.getItem(),
-            "&a单击打开" + getTitle()
+            "&aClick to open " + getTitle()
         ) : Converter.getItem(
             Material.MUSIC_DISC_5,
-            "&a单击打开" + getTitle()
+            "&aClick to open " + getTitle()
         );
         return Optional.of(item);
     }
@@ -77,7 +77,7 @@ public class LogiTechFalseRecipeSettingsGuideOption extends AbstractItemSettings
 
     @Override
     public String getTitle() {
-        return "&aFALSE 配方补全配置";
+        return "&aFALSE Recipe Completion Settings";
     }
 
     @Override
