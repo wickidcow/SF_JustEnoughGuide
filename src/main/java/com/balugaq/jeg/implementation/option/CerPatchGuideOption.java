48123ff421b37b272311c410adb3fd5c331ccc10
wickidcow <jaywago@gmail.com>
2026-09-19 11:02:45 -0400

    chore: translate value display option

--- src/main/java/com/balugaq/jeg/implementation/option/CerPatchGuideOption.java
+++ src/main/java/com/balugaq/jeg/implementation/option/CerPatchGuideOption.java
@@ -46,14 +46,14 @@ public class CerPatchGuideOption extends AbstractBooleanGuideOption {
     public ItemStack getDisplayItem(Player p, ItemStack guide, boolean enabled) {
         return Converter.getItem(
             isEnabled(p) ? Material.EMERALD : Material.REDSTONE,
-            "&b性价比显示: &" + (enabled ? "a启用" : "4禁用"),
+            "&bValue Comparison Display: &" + (enabled ? "aEnabled" : "4Disabled"),
             "",
-            "&7你现在可以选择是否",
-            "&7在搜索一个物品的全名的时候",
-            "&7在搜出的相关机器上显示",
-            "&7使用这个机器制作这个物品的性价比",
+            "&7Choose whether search results",
+            "&7show the estimated value/efficiency",
+            "&7of producing the exact searched item",
+            "&7with each compatible machine.",
             "",
-            "&7\u21E8 &e点击 " + (enabled ? "禁用" : "启用") + " 性价比显示"
+            "&7\u21E8 &e点击 " + (enabled ? "disable" : "enable") + " Value Comparison Display"
         );
     }
 
