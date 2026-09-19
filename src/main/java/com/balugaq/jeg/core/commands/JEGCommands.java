fcdda40c34297096435fc270f03a6939a8c065a5
wickidcow <jaywago@gmail.com>
2026-09-19 11:40:56 -0400

    core: use local ItemStack display-name helper

--- src/main/java/com/balugaq/jeg/core/commands/JEGCommands.java
+++ src/main/java/com/balugaq/jeg/core/commands/JEGCommands.java
@@ -42,7 +42,7 @@ import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.common.ChatColors;
 import io.github.thebusybiscuit.slimefun4.utils.ChestMenuUtils;
 import me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.ChestMenu;
-import net.guizhanss.minecraft.guizhanlib.gugu.minecraft.helpers.inventory.ItemStackHelper;
+import com.balugaq.jeg.utils.ItemStackHelper;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
