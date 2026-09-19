70d6904b9b6ccdfa9232ae39a7857fef18ab62b5
wickidcow <jaywago@gmail.com>
2026-09-19 10:54:59 -0400

    chore: translate recipe guide models to English

--- src/main/java/com/balugaq/jeg/utils/Models.java
+++ src/main/java/com/balugaq/jeg/utils/Models.java
@@ -27,44 +27,44 @@ import org.bukkit.inventory.ItemStack;
  * @since 1.3
  */
 public class Models {
-    public static final String RECIPE_COMPLETE_BOOK_MECHANISM_1 = "&a左键&e点击打开配方书";
-    public static final String RECIPE_COMPLETE_BOOK_MECHANISM_2 = "&a右键&e点击可再次补全";
-    public static final String RECIPE_COMPLETE_BOOK_MECHANISM_3 = "&e补全后再次&aShift+右键&e点击配方书补全64次";
-    public static final String RECIPE_COMPLETE_GUI_MECHANISM_1 = "&a左键&e点击物品补全1次";
-    public static final String RECIPE_COMPLETE_GUI_MECHANISM_2 = "&a右键&e点击物品补全64次";
+    public static final String RECIPE_COMPLETE_BOOK_MECHANISM_1 = "&aLeft-click &eto open the recipe book";
+    public static final String RECIPE_COMPLETE_BOOK_MECHANISM_2 = "&aRight-click &eto repeat the last completion";
+    public static final String RECIPE_COMPLETE_BOOK_MECHANISM_3 = "&eAfter completing once, &aShift + right-click &ethe recipe book to fill up to 64 sets";
+    public static final String RECIPE_COMPLETE_GUI_MECHANISM_1 = "&aLeft-click &ean item to fill 1 set";
+    public static final String RECIPE_COMPLETE_GUI_MECHANISM_2 = "&aRight-click &ean item to fill up to 64 sets";
     public static final ItemStack RTS_ITEM =
-        Converter.getItem(new SlimefunItemStack("_UI_RTS_ICON", Converter.getItem(Material.ANVIL, "&b实时搜索", "")));
+        Converter.getItem(new SlimefunItemStack("_UI_RTS_ICON", Converter.getItem(Material.ANVIL, "&bReal-Time Search", "")));
     public static final ItemStack SPECIAL_MENU_ITEM = Converter.getItem(new SlimefunItemStack(
-        "_UI_SPECIAL_MENU_ICON", Converter.getItem(Material.COMPASS, "&b超大配方", "", "&a点击打开超大配方(若有)")));
+        "_UI_SPECIAL_MENU_ICON", Converter.getItem(Material.COMPASS, "&bLarge Recipe", "", "&aClick to open the large recipe, when available")));
     public static final ItemStack INPUT_TEXT_ICON = Converter.getItem(new SlimefunItemStack(
         "_UI_RTS_INPUT_TEXT_ICON",
         Converter.getItem(
             Material.PAPER,
-            "&f搜索: &7在上方输入搜索词",
+            "&fSearch: &7enter a search term above",
             "&fTips:",
-            "&7 - &e左侧物品为返回键",
-            "&7 - &e中间物品为按键上一页",
-            "&7 - &e右侧物品为按键下一页"
+            "&7 - &eLeft item: back",
+            "&7 - &eMiddle item: previous page",
+            "&7 - &eRight item: next page"
         )
     ));
     public static final ItemStack JEG_GUIDE_GROUP = Converter.getItem(
-        new SlimefunItemStack("JEG_JEG_GUIDE_GROUP", Converter.getItem(Material.KNOWLEDGE_BOOK, "&bJEG 高阶指南书使用指南")));
+        new SlimefunItemStack("JEG_JEG_GUIDE_GROUP", Converter.getItem(Material.KNOWLEDGE_BOOK, "&bJEG Advanced Guide Help")));
     public static final ItemStack HIDDEN_ITEMS_GROUP = Converter.getItem(
-        new SlimefunItemStack("JEG_HIDDEN_ITEMS_GROUP", Converter.getItem(Material.BARRIER, "&c隐藏物品")));
+        new SlimefunItemStack("JEG_HIDDEN_ITEMS_GROUP", Converter.getItem(Material.BARRIER, "&cHidden Items")));
     public static final ItemStack NEXCAVATE_ITEMS_GROUP = Converter.getItem(new SlimefunItemStack(
-        "JEG_NEXCAVATE_ITEMS_GROUP_ICON", Converter.getItem(Material.BLACKSTONE, "&b文明复兴物品")));
+        "JEG_NEXCAVATE_ITEMS_GROUP_ICON", Converter.getItem(Material.BLACKSTONE, "&bNexcavate Items")));
     public static final ItemStack VANILLA_ITEMS_GROUP = Converter.getItem(
-        new SlimefunItemStack("JEG_VANILLA_ITEMS_GROUP", Converter.getItem(Material.CRAFTING_TABLE, "&7原版物品")));
+        new SlimefunItemStack("JEG_VANILLA_ITEMS_GROUP", Converter.getItem(Material.CRAFTING_TABLE, "&7Vanilla Items")));
     public static final ItemStack RECIPE_COMPLETABLE_GROUP = Converter.getItem(
-        new SlimefunItemStack("JEG_RECIPE_COMPLETABLE_GROUP", Converter.getItem(Material.CRAFTING_TABLE, "&b支持配方补全的机器")));
+        new SlimefunItemStack("JEG_RECIPE_COMPLETABLE_GROUP", Converter.getItem(Material.CRAFTING_TABLE, "&bRecipe-Compatible Machines")));
     public static final ItemStack JEG_ITEMS_GROUP = Converter.getItem(
-        new SlimefunItemStack("JEG_JEG_ITEMS_GROUP", Converter.getItem(Material.BOOK, "&b配方补全书")));
+        new SlimefunItemStack("JEG_JEG_ITEMS_GROUP", Converter.getItem(Material.BOOK, "&bRecipe Completion Book")));
     public static final ItemStack REPLACEMENT_CARDS_GROUP = Converter.getItem(
-        new SlimefunItemStack("JEG_REPLACEMENT_CARDS_GROUP", Converter.getItem(Material.PAPER, "&b替换卡 - 伴生物品")));
+        new SlimefunItemStack("JEG_REPLACEMENT_CARDS_GROUP", Converter.getItem(Material.PAPER, "&bReplacement Cards - Companion Items")));
     public static final ItemStack BANNED_ITEMS_GROUP = Converter.getItem(
-        new SlimefunItemStack("JEG_BANNED_ITEMS_GROUP", Converter.getItem(Material.COMMAND_BLOCK, "&c已禁用的物品")));
+        new SlimefunItemStack("JEG_BANNED_ITEMS_GROUP", Converter.getItem(Material.COMMAND_BLOCK, "&cDisabled Items")));
     public static final ItemStack MULTI_BLOCK_BUILDER_ITEMS_GROUP = Converter.getItem(
-        new SlimefunItemStack("MULTI_BLOCK_BUILDER_ITEMS_GROUP", Converter.getItem(Material.BRICKS, "&b多方块建筑物品")));
+        new SlimefunItemStack("MULTI_BLOCK_BUILDER_ITEMS_GROUP", Converter.getItem(Material.BRICKS, "&bMultiblock Building Items")));
     public static final ItemStack KEYBIND_ACTION_BORDER = Converter.getItem(
         Material.YELLOW_STAINED_GLASS_PANE, " ",
         " "
@@ -73,9 +73,9 @@ public class Models {
         "JEG_RECIPE_COMPLETE_BOOK",
         Converter.getItem(
             Material.SLIME_BALL,
-            "&b配方补全书",
+            "&bRecipe Completion Book",
             "",
-            "&f点击进行配方补全（使用方法见说明）",
+            "&fClick to start recipe completion (see the guide below)",
             RECIPE_COMPLETE_BOOK_MECHANISM_1,
             RECIPE_COMPLETE_BOOK_MECHANISM_2,
             RECIPE_COMPLETE_BOOK_MECHANISM_3
@@ -85,32 +85,32 @@ public class Models {
         "JEG_RECIPE_COMPLETE_USAGE_INFO",
         Converter.getItem(
             Material.PAPER,
-            "&a使用方法",
+            "&aHow to Use",
             "",
-            "&f1. &e将配方补全书放到你的物品栏里",
-            "&f2. &e右键打开任意一个适配配方补全的机器界面（如快捷机器）",
-            "&f3. &e然后左键点击配方补全书",
-            "&f4. &e选择你要补全的物品"
+            "&f1. &eKeep the Recipe Completion Book in your inventory",
+            "&f2. &eOpen a supported machine inventory",
+            "&f3. &eLeft-click the Recipe Completion Book",
+            "&f4. &eSelect the item whose recipe you want to fill"
         )
     );
     public static final SlimefunItemStack MECHANISM = new SlimefunItemStack(
         "JEG_RECIPE_COMPLETE_MECHANISM",
         Converter.getItem(
             Material.PAPER,
-            "&a机制",
+            "&aHow It Works",
             "",
-            "&7优先使用玩家背包中的物品进行补全配方",
-            "&7如果连接了网络，会尝试在网络中获取配方材料（仅网络拓展有效）",
-            "&7如果连接了AE网络，会尝试在AE网络中获取配方材料",
-            "&7连接解释: ",
-            "&7被进行配方补全的机器界面所对应的机器的东西南北上下任一方向紧贴的方块连接了网络/AE网络",
-            "&7即视为被进行配方补全的机器界面所对应的机器连接了网络/AE网络（不占用网络/AE网络节点）",
+            "&7Materials are taken from the player's inventory first",
+            "&7If a supported Networks system is connected, materials may be pulled from it",
+            "&7If a supported AE network is connected, materials may be pulled from it",
+            "&7Connection rules:",
+            "&7A network touching any face of the target machine is considered connected",
+            "&7The integration does not consume an additional network node",
             "",
-            "&9===配方书点击机制===",
+            "&9=== Recipe Book Controls ===",
             RECIPE_COMPLETE_BOOK_MECHANISM_1,
             RECIPE_COMPLETE_BOOK_MECHANISM_2,
             RECIPE_COMPLETE_BOOK_MECHANISM_3,
-            "&9===补全界面点击机制===",
+            "&9=== Completion Menu Controls ===",
             RECIPE_COMPLETE_GUI_MECHANISM_1,
             RECIPE_COMPLETE_GUI_MECHANISM_2
         )
@@ -119,67 +119,67 @@ public class Models {
         "JEG_RECIPE_COMPLETE_SUPPORTED_ADDONS_INFO",
         Converter.getItem(
             Material.PAPER,
-            "&a对以下附属的部分机器适配了配方补全",
-            "&7如需适配更多可在 JustEnoughGuide GitHub 提交 issue",
+            "&aRecipe completion supports selected machines from these addons",
+            "&7Request additional compatibility on the SF_JustEnoughGuide GitHub issue tracker",
             "",
-            "&7- &a多方块结构",
-            "&7- &a快捷机器",
-            "&7- &a乱序技艺 2.0-Preview",
-            "&7- &a乱序技艺 2.0",
-            "&7- &a乱序技艺 2.0 改版",
-            "&7- &a无尽贪婪",
-            "&7- &a无尽贪婪2",
-            "&7- &a逻辑工艺",
-            "&7- &a网络",
-            "&7- &a网络拓展",
-            "&7- &a黑曜石科技",
-            "&7- &a粘液AE",
-            "&7- &a蓬松机器",
-            "&7- &a粘液匠魂",
-            "&7- &a星系",
-            "&7- &a美食家",
-            "&7- &aRyken自定义附属",
-            "&7- &a基岩科技",
-            "&7- &a炼金术自传",
-            "&7- &a粘土科技",
-            "&7- &a无底存储",
-            "&7- &a简易工具",
-            "&7- &a农耕工艺",
-            "&7- &a化学工程",
-            "&7- &a无尽压缩",
-            "&7- &a魔法",
-            "&7- &a青山科技",
-            "&7- &a迷狱生机"
+            "&7- &aSlimefun multiblocks",
+            "&7- &aFastMachines",
+            "&7- &aFinalTECH 2.0 Preview",
+            "&7- &aFinalTECH 2.0",
+            "&7- &aFinalTECH 2.0 改版",
+            "&7- &aInfinityExpansion",
+            "&7- &aInfinityExpansion2",
+            "&7- &aLogiTech",
+            "&7- &aNetworks",
+            "&7- &aNetworks拓展",
+            "&7- &aObsidianExpansion",
+            "&7- &aSlimeAE",
+            "&7- &aFluffyMachines",
+            "&7- &aSlimeTinker",
+            "&7- &aGalactifun",
+            "&7- &aGastronomicon",
+            "&7- &aRykenSlimefunCustomizer",
+            "&7- &aBedrockTechnology",
+            "&7- &aAlchimiaVitae",
+            "&7- &aClayTech",
+            "&7- &aDankTech",
+            "&7- &aSimpleUtils",
+            "&7- &aCultivation",
+            "&7- &aElementManipulation",
+            "&7- &aInfinityCompress",
+            "&7- &aMagic",
+            "&7- &aTsingshanTechnology",
+            "&7- &aWilderNether"
         )
     );
 
     public static final SlimefunItemStack JEG_RECIPE_COMPLETE_BUTTON = new SlimefunItemStack(
         "JEG_RECIPE_COMPLETE_BUTTON",
         Material.KNOWLEDGE_BOOK,
-        "&6配方补全",
-        "&7点击打开配方补全界面"
+        "&6Recipe Completion",
+        "&7Click to open recipe completion"
     );
 
     public static final ItemStack ITEM_MARK_BACKGROUND = Converter.getItem(
         Material.GREEN_STAINED_GLASS_PANE,
-        "&a&l添加收藏物",
+        "&a&lAdd Bookmark",
         "",
-        "&7左键物品添加到收藏中"
+        "&7Left-click an item to add it to bookmarks"
     );
 
     public static final ItemStack SLIMEFUN_RECIPE_EDIT = Converter.getItem(
         Material.DIAMOND,
-        "&a&lSlimeFunRecipe 配方编辑器",
+        "&a&lSlimeFunRecipe Recipe Editor",
         "",
-        "&e点击打开配方编辑器"
+        "&eClick to open the recipe editor"
     );
 
     public static final SlimefunItemStack CUSTOM_LAG_BLOCK = new SlimefunItemStack(
         "JEG_CUSTOM_LAG_BLOCK",
         Converter.getItem(
             Material.COMMAND_BLOCK,
-            "&a自定义延时器",
-            "&a放置后打开界面，可设定该机器粘液刻耗时",
-            "&c仅 OP 可使用"
+            "&aCustom Timer",
+            "&aPlace and open this item to configure the machine's Slimefun tick delay",
+            "&cOperators only"
     ));
 }
