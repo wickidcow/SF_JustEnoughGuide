af725ae42ad8c09dea1c6f7c214d5c786c5f4827
wickidcow <jaywago@gmail.com>
2026-09-19 11:41:49 -0400

    core: use local ItemStack display-name helper

--- src/main/java/com/balugaq/jeg/api/objects/collection/data/MachineData.java
+++ src/main/java/com/balugaq/jeg/api/objects/collection/data/MachineData.java
@@ -52,7 +52,7 @@ import io.github.thebusybiscuit.slimefun4.libraries.dough.collections.Randomized
 import it.unimi.dsi.fastutil.ints.IntList;
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.abstractItems.AContainer;
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.abstractItems.MachineRecipe;
-import net.guizhanss.minecraft.guizhanlib.gugu.minecraft.helpers.inventory.ItemStackHelper;
+import com.balugaq.jeg.utils.ItemStackHelper;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.Nullable;
