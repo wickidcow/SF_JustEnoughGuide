47aef936504183832d80a75ece0ce87eb0bbe9af
wickidcow <jaywago@gmail.com>
2026-09-19 11:03:46 -0400

    chore: translate multiblock builder messages

--- src/main/java/com/balugaq/jeg/implementation/items/MultiBlockBuilderItem.java
+++ src/main/java/com/balugaq/jeg/implementation/items/MultiBlockBuilderItem.java
@@ -54,9 +54,9 @@ public class MultiBlockBuilderItem extends JEGSlimefunItem implements NotPlaceab
                 if (!player.isOp() && player.getGameMode() != GameMode.CREATIVE) {
                     e.getItem().setAmount(e.getItem().getAmount() - 1);
                 }
-                player.sendMessage(ChatColors.color("&a多方块 " + multiBlock.getSlimefunItem().getItemName() + " 搭建成功！"));
+                player.sendMessage(ChatColors.color("&aMultiblock " + multiBlock.getSlimefunItem().getItemName() + " built successfully!"));
             } else {
-                player.sendMessage(ChatColors.color("&c多方块 " + multiBlock.getSlimefunItem().getItemName() + " 搭建失败！"));
+                player.sendMessage(ChatColors.color("&cMultiblock " + multiBlock.getSlimefunItem().getItemName() + " could not be built."));
             }
         }));
     }
